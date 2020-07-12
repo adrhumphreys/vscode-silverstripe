7c0b613ffadbac4c34f7a25db1563ec5b4fd9e05
Gorrie Coe <gorriecoe@gmail.com>
2020-07-13 00:32:31 +1200

    Fixed comments, added snippet details and compiled

--- src/silverstripeCompletionProvider.ts
+++ src/silverstripeCompletionProvider.ts
@@ -17,15 +17,21 @@ export class silverstripeCompletionProvider implements vscode.CompletionItemProv
       )
 
       suggestion.insertText = new vscode.SnippetString(
-        this.sanchez.comments ? snippet.suggestion.body + snippet.suggestion.body : snippet.suggestion.body
+        this.sanchez.data.comments ? snippet.suggestion.comment + snippet.suggestion.body : snippet.suggestion.body
       );
 
+      suggestion.detail = [
+        snippet.suggestion.information,
+        snippet.suggestion.description,
+        snippet.suggestion.url
+      ].join('\n\n');
 
-      suggestion.documentation = snippet.suggestion.documentation;
-
-      // Insert
+      // Get a list of locations to safely apply use items from sanchez.
+      // Then insert in the given locations.
       suggestion.additionalTextEdits = this.sanchez.getUseItemLoc({
+        // Pass through the current editor view contents.
         text: document.getText(),
+        // Pass the useItems set specified in the suggestion.
         useItems: snippet.suggestion.useItems
       }).map(useItem => {
         return new vscode.TextEdit(
